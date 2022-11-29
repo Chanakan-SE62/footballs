cb75f45cca6dc522e5367e5cf73da0b0bba260fe
Chanakan-SE62 <59428866+Chanakan-SE62@users.noreply.github.com>
2022-11-30 01:45:53 +0700

    card content

---
+++ components/card_group.php
@@ -0,0 +1,193 @@
+<section class="container">
+    <div class="card">
+        <h5 class="card-title-group">A</h5>
+        <div class="card-country-group">
+            <img src="./img/A1-qatar.jpg" alt="qatar flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>กาตาร์</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/A2-ecuador.png" alt="ecuador flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เอกวาดอร์</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/A3-senegal.png" alt="senegal flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เซเนกัล</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/A4-netherlands.png" alt="netherlands flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เนเธอร์แลนด์</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">B</h5>
+        <div class="card-country-group">
+            <img src="./img/B1-england.png" alt="england flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>อังกฤษ</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/B2-iran.jpg" alt="iran flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>อิหร่าน</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/B3-united.png" alt="united flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>สหรัฐอเมริกา</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/B4-wales.png" alt="wales flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เวลส์</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">C</h5>
+        <div class="card-country-group">
+            <img src="./img/C1-argentina.png" alt="argentina flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>อาร์เจนตินา</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/C2-saudiArabia.png" alt="saudiArabia flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>ซาอุดีอาระเบีย</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/C3-mexico.png" alt="mexico flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เม็กซิโก</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/C4-poland.png" alt="poland flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>โปแลนด์</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">D</h5>
+        <div class="card-country-group">
+            <img src="./img/D1-france.png" alt="france flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>ฝรั่งเศส</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/D2-australia.png" alt="australia flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>ออสเตรเลีย</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/D3-denmark.png" alt="denmark flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เดนมาร์ก</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/D4-tunisia.png" alt="tunisia flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>ตูนิเซีย</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">E</h5>
+        <div class="card-country-group">
+            <img src="./img/E1-spain.jpg" alt="spain flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>สเปน</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/E2-costaRica.png" alt="costaRica flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>คอสตาริกา</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/E3-germany.png" alt="germany flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เยอรมนี</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/E4-japan.png" alt="japan flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>ญี่ปุ่น</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">F</h5>
+        <div class="card-country-group">
+            <img src="./img/F1-belgium.png" alt="belgium flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เบลเยียม</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/F2-cannada.jpg" alt="cannada flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>แคนาดา</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/F3-morocco.png" alt="morocco flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>โมร็อกโก</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/F4-croatia.png" alt="croatia flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>โครเอเชีย</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">G</h5>
+        <div class="card-country-group">
+            <img src="./img/G1-brazil.png" alt="brazil flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>บราซิล</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/G2-serbia.png" alt="serbia flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เซอร์เบีย</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/G3-switzerland.png" alt="switzerland flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>สวิตเซอร์แลนด์</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/G4-cameroon.png" alt="cameroon flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>แคเมอรูน</span>
+        </div>
+    </div>
+
+    <div class="card">
+        <h5 class="card-title-group">H</h5>
+        <div class="card-country-group">
+            <img src="./img/H1-portugal.png" alt="portugal flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>โปรตุเกส</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/H2-ghana.png" alt="ghana flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>กานา</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/H3-uruguay.png" alt="uruguay flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>อุรุกวัย</span>
+        </div>
+        <div class="card-country-group">
+            <img src="./img/H4-korean.jpg" alt="korean flag" class="flag-img">
+            &nbsp; &nbsp; &nbsp;
+            <span>เกาหลีใต้</span>
+        </div>
+    </div>
+</section>
