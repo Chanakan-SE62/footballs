65ab08d1f01ae357a9e6ad30bd70804076055403
Chanakan-SE62 <59428866+Chanakan-SE62@users.noreply.github.com>
2022-11-30 03:53:28 +0700

    table index

---
+++ components/table.php
@@ -0,0 +1,93 @@
+<div class="divTable">
+    <h3 class="text-table-title" id="tableIndex">ตารางการแข่งขัน (รอบแบ่งกลุ่ม)</h3>
+    <div class="dropdown button-filter">
+        <button class="btn btn-filter dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            เรียงตาม
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">ทั้งหมด</a></li>
+            <li><a class="dropdown-item" href="#">วันนี้</a></li>
+            <li><a class="dropdown-item" href="#">พรุ่งนี้</a></li>
+        </ul>
+    </div>
+    <br>
+
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">ลำดับ</th>
+                <th scope="col">วันที่</th>
+                <th scope="col">เวลา</th>
+                <th scope="col">กลุ่ม</th>
+                <th scope="col">ทีม</th>
+                <th scope="col">ผลคะแนน</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>30/11/2565</td>
+                <td>02.00 น.</td>
+                <td>B</td>
+                <td>อิหร่าน VS สหรัฐอเมริกา</td>
+                <td>0 - 1 (อยู่ระหว่างการแข่งขัน)</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>30/11/2565</td>
+                <td>02.00 น.</td>
+                <td>B</td>
+                <td>เวลส์ VS อังกฤษ</td>
+                <td>0 - 2 (อยู่ระหว่างการแข่งขัน)</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>30/11/2565</td>
+                <td>22.00 น.</td>
+                <td>D</td>
+                <td>ตูนิเซีย VS ฝรั่งเศส</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>30/11/2565</td>
+                <td>22.00 น.</td>
+                <td>D</td>
+                <td>ออสเตรเลีย VS เดนมาร์ก</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>31/11/2565</td>
+                <td>02.00 น.</td>
+                <td>C</td>
+                <td>โปแลนด์ VS อาร์เจนตินา</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td>31/11/2565</td>
+                <td>02.00 น.</td>
+                <td>C</td>
+                <td>ซาอุดีอาระเบีย VS เม็กซิโก</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td>31/11/2565</td>
+                <td>22.00 น.</td>
+                <td>F</td>
+                <td>โครเอเชีย VS เบลเยียม</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td>31/11/2565</td>
+                <td>22.00 น.</td>
+                <td>C</td>
+                <td>แคนาดา VS โมร็อกโก</td>
+                <td>-</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
